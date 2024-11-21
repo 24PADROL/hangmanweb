763617ea694417b837f61d3eebcb7ee33116c7a7
24PADROL <antho.yaris@gmail.com>
2024-11-21 12:15:38 +0100

    Arrangement des fichier

---
+++ hangmanweb/init.go
@@ -0,0 +1,50 @@
+package hangmanweb
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+)
+
+type DataForm struct {
+	LettreUsed []string
+	Words      []string
+	NameFill   string
+	Word       string
+}
+
+var Data DataForm
+
+var nameFill string = "motsimple.txt"
+
+func randomWord() {
+	fichier, err := os.Open(nameFill)
+	if err != nil {
+		fmt.Println("Erreur:", err)
+		return
+	}
+
+	defer fichier.Close()
+	scanner := bufio.NewScanner(fichier)
+	for scanner.Scan() {
+		Data.Words = append(Data.Words, scanner.Text())
+	}
+	if len(Data.Words) == 0 {
+		fmt.Println("le fichier ne contient rien")
+		return
+	}
+	Data.Word = Data.Words[rand.Intn(200)]
+}
+func Init() {
+	randomWord()
+}
+func Web(){
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/input", Input)
+	fmt.Println("(http://localhost:8080) - server started on port", port)
+	http.ListenAndServe(port, nil)
+	fs := http.FileServer(http.Dir("serv/"))
+	http.Handle("serv/", http.StripPrefix("serv/", fs))
+}
