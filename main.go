d5736660fbea504c85820007a3b696b7703e874a
24PADROL <antho.yaris@gmail.com>
2024-12-09 11:29:59 +0100

    AAAAHHHH

--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
 
-<<<<<<< HEAD
-import (
-	h "hangmanweb/hangmanweb"
-)
-
 
 func main() {
 	h.Init()
@@ -15,5 +10,4 @@ const port = ":8080"
 func main() {
 	Init()
 	Web()
->>>>>>> 97d0551af001477653fecaedd3a55809a12a3847
 }
