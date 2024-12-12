6f91ab06f0fb5654001ba7e5fb7e068ca90f7b6a
24PADROL <antho.yaris@gmail.com>
2024-12-12 09:22:36 +0100

    on me force de commit

--- hangmanweb/init.go
+++ hangmanweb/init.go
@@ -60,18 +60,12 @@ func Init() {
 func Web() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/input", Input)
+	http.HandleFunc("/victory", Victory)
+	http.HandleFunc("/reset", Reset)
 	fmt.Println("(http://localhost:8080) - server started on port", port)
 	http.ListenAndServe(port, nil)
 	fs := http.FileServer(http.Dir("serv/"))
 	http.Handle("serv/", http.StripPrefix("serv/", fs))
-}
-
-func WebVictory(){
-	http.HandleFunc("/victory", Victory)
-	http.ListenAndServe(port, nil)
-	fs := http.FileServer(http.Dir("serv/"))
-	http.Handle("serv/", http.StripPrefix("serv/", fs))
-	fmt.Println("A")
 }
 
 func PrintWeb(){}
