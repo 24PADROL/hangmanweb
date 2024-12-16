b70706ccc9d9eccc23190ea663ba1e75c0e09379
24PADROL <antho.yaris@gmail.com>
2024-12-16 11:23:53 +0100

    soucis

--- hangmanweb/handler.go
+++ hangmanweb/handler.go
@@ -26,48 +26,60 @@ func Lose(w http.ResponseWriter, r *http.Request) {
 	RenderTemplate(w, "lose")
 }
 
+func letterAlreadyGuessed(s string) bool {
+	for _, i := range Data.LettreUsed {
+		if s == i {
+			return true
+		}
+	}
+	return false
+}
+
+func formateLetter(guessedLetter string) string{
+	if guessedLetter == "é" || guessedLetter == "è" || guessedLetter == "ë" || guessedLetter == "ê" {
+		guessedLetter = "e"
+	}
+	if guessedLetter == "à" || guessedLetter == "â" {
+		guessedLetter = "a"
+	}
+	if guessedLetter == "ù" || guessedLetter == "û" {
+		guessedLetter = "u"
+	}
+	if guessedLetter == "î" || guessedLetter == "ï" {
+		guessedLetter = "i"
+	}
+	if guessedLetter == "ô" || guessedLetter == "ö" {
+		guessedLetter = "o"
+	}
+	if guessedLetter == "ç" {
+		guessedLetter = "c"
+	}
+	return guessedLetter
+}
+
 func Input(w http.ResponseWriter, r *http.Request) {
 	// Retrieve the guessed letter
 	guessedLetter := r.FormValue("LettreARecuperer")
-	// if guessedLetter == "" {
-	// 	http.Error(w, "No letter provided", http.StatusBadRequest)
-	// 	return
-	// }
-	// faire tout ls carac de fdp
-	if guessedLetter == "é"||guessedLetter == "è"{
-		guessedLetter = "e"
-	}
-	if guessedLetter == "à"{
-		guessedLetter = "a"
-	}
-	if guessedLetter == "ù"{
-		guessedLetter = "u"
-	}
-
-	Data.LettreUsed = append(Data.LettreUsed, guessedLetter)
-
-	// Check if the guessed letter exists in the word
-	for i, char := range Data.Word {
-		if string(char) == guessedLetter {
-			// Reveal the guessed letter in the hidden word
-			Data.TabHidden[2*i] = guessedLetter
-			nothere = false
+	guessedLetter = formateLetter(guessedLetter)
+	if !letterAlreadyGuessed(guessedLetter) {
+		Data.LettreUsed = append(Data.LettreUsed, guessedLetter)
+		for i, char := range Data.Word {
+			if string(char) == guessedLetter {
+				// Reveal the guessed letter in the hidden word
+				Data.TabHidden[2*i] = guessedLetter
+				nothere = false
+			}
 		}
-	}
-	// if strings.Join(Data.TabHidden, "") == Data.Word {
-	// 	win = true
-	// 	http.Redirect(w, r, "/victory", http.StatusSeeOther)
-	// 	return
-	// }
-	if nothere {
-		
-		Data.Try--
-	}
-	win = true
-	for _, i := range Data.TabHidden {
-		if i == "_" { // If any element is "_", the game is not won
-			win = false
-			break
+		if nothere {
+			Data.Try--
+		}
+		nothere = true
+		win = true
+		for _, i := range Data.TabHidden {
+			if i == "_" { // If any element is "_", the game is not won
+				win = false
+				break
+			}
 		}
 	}
 	if win {
